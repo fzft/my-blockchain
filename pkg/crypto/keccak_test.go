61c954db1c0f58c0eb8cc424d47fc653b2a8f4eb
fzft <578847278@qq.com>
2023-02-22 19:55:20 +0800

    keccak

--- pkg/crypto/keccak_test.go
+++ pkg/crypto/keccak_test.go
@@ -1,16 +1,2 @@
 package crypto
 
-import (
-	"testing"
-)
-
-func TestNewKeccak_padding(t *testing.T) {
-	message := []byte("The quick brown fox jumps over the lazy dog")
-
-	k := NewKeccak(message)
-
-	k.padding()
-
-	t.Logf("paddedMessage len: %+v", len(k.paddedMessage))
-
-}
